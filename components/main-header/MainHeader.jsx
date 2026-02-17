c3f60c2e397ff144dc807f1e5d722d5df20f8744
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-02-17 14:16:33 +0200

    chore : spliting into separete components

---
+++ components/main-header/MainHeader.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import LogoImg from "@/assets/logo.png";
+import classes from "./MainHeader.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "./MainHeaderBackground";
+function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href={"/"} className={classes.logo}>
+          <Image src={LogoImg.src} alt="logo image" priority />
+          NextLevel food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href={"/meals"}>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href={"/community"}>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+export default MainHeader;
