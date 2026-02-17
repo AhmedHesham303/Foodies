561f6bb232d66acf5c524e3107a5598cb68deda7
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-02-17 22:15:17 +0200

    chore : splited nav-link into separate client component

--- components/main-header/MainHeader.jsx
+++ components/main-header/MainHeader.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import LogoImg from "@/assets/logo.png";
-import classes from "./MainHeader.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./MainHeaderBackground";
+import NavLink from "./NavLink";
+import classes from "./MainHeader.module.css";
 function MainHeader() {
   return (
     <>
@@ -15,10 +16,10 @@ function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href={"/meals"}>Browse Meals</Link>
+              <NavLink href={"meals"}>Browse Meals</NavLink>
             </li>
             <li>
-              <Link href={"/community"}>Foodies Community</Link>
+              <NavLink href={"community"}>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
---
+++ components/main-header/NavLink.jsx
@@ -0,0 +1,23 @@
+"use client";
+import { usePathname } from "next/navigation";
+import classes from "./nav-link.module.css";
+
+import Link from "next/link";
+function NavLink({ href, children }) {
+  const currentPath = usePathname();
+
+  return (
+    <Link
+      href={`/${href}`}
+      className={
+        currentPath.startsWith(`/${href}`)
+          ? `${classes.link} ${classes.active}`
+          : `${classes.link}`
+      }
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default NavLink;
