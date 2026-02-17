b8b53bd8f9582f914e2eaa15e5b24ce997e921b9
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-02-17 12:08:05 +0200

    chore :  added  CSS module to style with.

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import LogoImg from "@/assets/logo.png";
+import classes from "./MainHeader.module.css";
 function MainHeader() {
   return (
-    <header>
-      <Link href={"/"}>
+    <header className={classes.header}>
+      <Link href={"/"} className={classes.logo}>
         <img src={LogoImg.src} alt="logo image" />
         NextLevel food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href={"/meals"}>Browse Meals</Link>
