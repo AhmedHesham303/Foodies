8789c8596f124365d88896dae6b6e57a5ab03d8b
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-02-18 10:39:54 +0200

    feat : linked with sqlite3

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,5 +1,24 @@
-function page() {
-  return <div>meals</div>;
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/MealsGrid";
+function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals,created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favourite recipe and cook it yourself</p>
+        <p className={classes.cta}>
+          <Link href={"/share"}>Share your recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
 
-export default page;
+export default MealsPage;
