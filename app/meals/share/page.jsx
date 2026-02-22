6ffc1c5bbf63be22c1f60ce3faf0c7b2ce7be2bb
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-02-22 09:26:01 +0200

    added server-side form validation

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,6 +1,5 @@
-import ImagePicker from "@/components/meals/ImagePicker";
 import classes from "./page.module.css";
-import { shareMeal } from "@/lib/actions";
+import MealsForm from "@/components/meals/MealsForm";
 
 export default function ShareMealPage() {
   return (
@@ -12,39 +11,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
-          <div className={classes.row}>
-            <p>
-              <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
-            </p>
-            <p>
-              <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
-            </p>
-          </div>
-          <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
-          </p>
-          <p>
-            <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
-          </p>
-          <p>
-            <label htmlFor="instructions">Instructions</label>
-            <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
-              required
-            ></textarea>
-          </p>
-          <ImagePicker label={"Your Image"} name={"image"} />
-          <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
-          </p>
-        </form>
+        <MealsForm />
       </main>
     </>
   );
---
+++ components/meals/MealsForm.jsx
@@ -0,0 +1,54 @@
+"use client";
+import ImagePicker from "./ImagePicker";
+import classes from "./../../app/meals/share/page.module.css";
+import { useActionState } from "react";
+import { shareMeal } from "@/lib/actions";
+
+function MealsForm() {
+  const [state, formAction, pending] = useActionState(shareMeal, null);
+
+  return (
+    <form className={classes.form} action={formAction}>
+      <div className={classes.row}>
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input type="text" id="name" name="name" required />
+        </p>
+        <p>
+          <label htmlFor="email">Your email</label>
+          <input type="email" id="email" name="email" required />
+        </p>
+      </div>
+
+      <p>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" required />
+      </p>
+
+      <p>
+        <label htmlFor="summary">Short Summary</label>
+        <input type="text" id="summary" name="summary" required />
+      </p>
+
+      <p>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+          rows="10"
+          required
+        ></textarea>
+      </p>
+
+      <ImagePicker label="Your Image" name="image" />
+
+      <p className={classes.actions}>
+        <button type="submit" disabled={pending}>
+          {pending ? "Submitting..." : "Share Meal"}
+        </button>
+      </p>
+    </form>
+  );
+}
+
+export default MealsForm;
